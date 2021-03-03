ae0c789d2c9be3ff2591121732185552e0216a23
abdelrahmansaf <abdel@gmail.com>
2021-03-03 11:44:43 +0100

    telechargement , supprimer if existe

--- function_tam.py
+++ function_tam.py
@@ -2,8 +2,11 @@ import wget
 import pandas
 import logging
 import os
-if "TAM_MMM_TpsReel.csv" :
+from glob import glob
+
 def download():
+    for filename in glob("*.csv"):
+        os.remove(filename)
     url='https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv'
     wget.download(url)
     csv=pandas.read_csv('TAM_MMM_TpsReel.csv',sep=';')
@@ -13,7 +16,6 @@ def stations():
     df=download()
     stations=set(df['stop_name'].tolist())
     return stations
-
 
   
 
